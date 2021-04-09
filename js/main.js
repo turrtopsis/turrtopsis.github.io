4c072449d85c817832b614c00ffd183abbb34ce6
turrtopsis <53790947+turrtopsis@users.noreply.github.com>
2021-04-09 19:59:33 +0300

    Добавили правила для адаптива

--- js/main.js
+++ js/main.js
@@ -1,20 +1,44 @@
 const swiperChannel = new Swiper('.channel-slider', {
     // Optional parameters
     loop: true,
-    slidesPerView: 6,
-  
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+      1900: {
+        slidesPerView: 6,
+      },
+      1600: {
+        slidesPerView: 5,
+      },
+      1300: {
+        slidesPerView: 4,
+      },
+      1100: {
+        slidesPerView: 3,
+      },
+      800: {
+        slidesPerView: 2,
+      },
+    },
     // Navigation arrows
     navigation: {
       nextEl: '.channel-button-next',
-      prevEl: '.channel-button-prev',
+      prevEl: '.channel-button-prev', 
     },
 });
-
 const swiperRecommended = new Swiper('.recommended-slider', {
     // Optional parameters
     loop: true,
-    slidesPerView: 3,
-  
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+      1600: {
+        slidesPerView: 3,
+      },
+      1100: {
+        slidesPerView: 2,
+      },
+    },
     // Navigation arrows
     navigation: {
       nextEl: '.recommended-button-next',
@@ -24,7 +48,25 @@ const swiperRecommended = new Swiper('.recommended-slider', {
 const swiperFood = new Swiper('.food-slider', {
     // Optional parameters
     loop: true,
-    slidesPerView: 6,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+      1900: {
+        slidesPerView: 6,
+      },
+      1600: {
+        slidesPerView: 5,
+      },
+      1300: {
+        slidesPerView: 4,
+      },
+      1100: {
+        slidesPerView: 3,
+      },
+      800: {
+        slidesPerView: 2,
+      },
+    },
   
     // Navigation arrows
     navigation: {
@@ -32,3 +74,15 @@ const swiperFood = new Swiper('.food-slider', {
       prevEl: '.food-button-prev',
     },
 });
+
+const searchBtn = document.querySelector('.mobile-search');
+const mobileSearch = document.querySelector('.input-group');
+searchBtn.addEventListener('click', () => {
+  mobileSearch.classList.toggle('is-open');
+});
+
+if (document.documentElement.scrollWidth <= 640) {
+  swiperChannel.destroy(),
+  swiperRecommended.destroy(),
+  swiperFood.destroy();
+};
